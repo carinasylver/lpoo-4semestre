6507061ec4cc4a28cbdfe70f9b6a9f6fcedf04dd
carinasylver <cah.sylver@gmail.com>
2024-08-17 10:33:50 -0300

    conceitos de classe, atributos, métodos e visibilidade das classes

---
+++ obj1-criacaoDeClasses-instanciacaoDeObjetos-manipulacaoDeColecoes-e-encapsulamento/src/conceito1classe/Pessoa.java
@@ -0,0 +1,15 @@
+package conceito1classe;
+
+public class Pessoa {
+    private String cpf;
+    private String nome;
+    private int idade;
+
+    /*-------CONCEITOS DE UMA CLASSE
+    * Pessoa eh o nome da classe e comeca sempre com letra maiuscula
+    * private eh a visibilidade da classe
+    * String eh o tipo do atributo
+    * cpf eh o atributo
+    */
+
+}
---
+++ obj1-criacaoDeClasses-instanciacaoDeObjetos-manipulacaoDeColecoes-e-encapsulamento/src/conceito2ClasseComMetodos2/Pessoa.java
@@ -0,0 +1,19 @@
+package conceito2ClasseComMetodos2;
+
+public class Pessoa {
+    private String cpf;
+    private String nome;
+    private int idade;
+
+    public void pensar() {
+        System.out.println("Pessoa pensando");
+    }
+
+    /*-------CONCEITOS DE UMA CLASSE
+     * Pessoa eh o nome da classe e comeca sempre com letra maiuscula
+     * private eh a visibilidade da classe
+     * String eh o tipo do atributo
+     * cpf eh o atributo
+     * pensar() eh o metodo
+     */
+}
